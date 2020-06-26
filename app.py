57ac8ade010a68c5a840d1509a954eecb0909b5f
Cristian Mayco da Costa <cristianmayco@gmail.com>
2020-06-25 21:36:18 -0300

    Added support JWT

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'keysupersecret'
 api = Api(app)
+
+JWT = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item, 200
-        return {'item': None}, 404
+        item = next(filter(lambda i: i['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        if next(filter(lambda i: i['name'] == name, items), None) is not None:
+            return {'message': f'An item with name {name} already exists'}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
